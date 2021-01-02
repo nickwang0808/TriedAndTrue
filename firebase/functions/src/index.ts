fd72fb834e183284d34f9223dcc659679f2627aa
Nick Wang <wang703003515@gmail.com>
2021-01-02 06:46:00 +0000

    auth might be done

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,20 +1,22 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+admin.initializeApp();
 
 export const customClaims = functions.https.onCall(async (_, context) => {
   const claims = context.auth?.token;
-  if (!claims)
+  const uid = context.auth?.uid;
+  if (!claims || !uid)
     throw new functions.https.HttpsError("unauthenticated", "please login");
 
   const hasuraClaims = {
     "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": ["user"],
       "x-hasura-default-role": "user",
-      "x-hasura-user-id": "1234567890",
+      "x-hasura-allowed-roles": ["user", "admin"],
+      "x-hasura-user-id": uid,
     },
   };
 
-  await admin.auth().setCustomUserClaims(claims.sub, hasuraClaims);
+  await admin.auth().setCustomUserClaims(uid, hasuraClaims);
 
   return true;
 });
@@ -22,5 +24,29 @@ export const customClaims = functions.https.onCall(async (_, context) => {
 export const newUserRecordWrite = functions.auth
   .user()
   .onCreate(async (user) => {
-    // write to db
+    const { uid, email, photoURL, displayName } = user;
+
+    const admin_secret = "myadminsecretkey";
+    const url = "http://35.220.182.160:8080/v1/graphql";
+    const query = `mutation ($id: String!, $name: String, $imgURL: String, $email: String!) {
+        insert_user(objects: [{email: $email, id: $id, img: $imgURl, name: $name}], on_conflict: {constraint: user_pkey, update_columns: [name, email, img]}) {
+          affected_rows
+        }
+      }
+      `;
+
+    const variables = { id: uid, name: displayName, imgURL: photoURL, email };
+
+    const result = await fetch(url, {
+      headers: {
+        "content-type": "application/json",
+        "x-hasura-admin-secret": admin_secret,
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
+    }).then((res) => res.json());
+
+    console.log(result);
   });
