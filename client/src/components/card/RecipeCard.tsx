5871cbc69acaebc69835d57219ce38145607f240
Nick Wang <wang703003515@gmail.com>
2021-01-03 09:03:38 +0000

    card style fixed

--- client/src/components/card/RecipeCard.tsx
+++ client/src/components/card/RecipeCard.tsx
@@ -11,23 +11,32 @@ import cookTimeCLock from "../../assets/svg/cookTimeClock.svg";
 
 export default function RecipeCard() {
   return (
-    <StyledCard>
+    <StyledCard mode="md">
       <StyledImg src="https://picsum.photos/200/300" />
-      <IonCardHeader>
-        <IonCardTitle>Hometown Grilled Hamburgers</IonCardTitle>
-        <IonCardSubtitle>
-          <IonIcon icon={cookTimeCLock} /> 45min
+      <StyledIonHeader mode="md">
+        <IonCardTitle mode="md">Hometown Grilled Hamburgers</IonCardTitle>
+        <IonCardSubtitle mode="md">
+          <IonIcon mode="md" icon={cookTimeCLock} /> 45min
         </IonCardSubtitle>
-      </IonCardHeader>
+      </StyledIonHeader>
     </StyledCard>
   );
 }
 
 const StyledCard = styled(IonCard)`
   margin: 0;
+  border-radius: 0;
+
+  border: 1px solid #dcdcdc;
+  box-shadow: none;
 `;
 const StyledImg = styled.img`
   object-fit: cover;
   width: 100%;
   height: 135px;
+  margin: 1px;
+`;
+
+const StyledIonHeader = styled(IonCardHeader)`
+  padding: 8px;
 `;
