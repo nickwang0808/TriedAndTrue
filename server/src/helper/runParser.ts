5cd335e4f589d26993fbf2375867538047160d01
Nick Wang <63834422+nickwang0808@users.noreply.github.com>
2021-02-04 12:08:11 +0800

    added pre formatter (#55)
    * fixed #24
    
    * missed code commit again
    
    * added pre formatting for parser

--- server/src/helper/runParser.ts
+++ server/src/helper/runParser.ts
@@ -1,4 +1,5 @@
 import { exec } from "child_process";
+import { parse } from "recipe-ingredient-parser-v2";
 import { promisify } from "util";
 
 const promiseExec = promisify(exec);
@@ -13,7 +14,15 @@ export interface IParsedIngredinets {
 }
 
 export default async function runParser(input: string[]) {
-  const ingredients = input.join("\n");
+  //  use the regex ingredient parser first to format some of the odd wording
+  const formattedInputs = input.map((text) => {
+    const { quantity, unit, ingredient } = parse(text);
+    return `${quantity ?? ""} ${unit ?? ""} ${ingredient ?? ""}`
+      .trim()
+      .replace(/\s+/g, " ");
+  });
+
+  const ingredients = formattedInputs.join("\n");
 
   const { stdout, stderr } = await promiseExec(
     `docker run parser ./parse "${ingredients}"`
