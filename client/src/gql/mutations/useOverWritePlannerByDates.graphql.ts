c1fc5e8f9ab3499ee97130f3dc49f2eb981f3e82
Nick Wang <wang703003515@gmail.com>
2021-02-04 08:27:17 +0000

    extracted mutation function with update function to its corresponding custom hook

--- client/src/gql/mutations/useOverWritePlannerByDates.graphql.ts
+++ client/src/gql/mutations/useOverWritePlannerByDates.graphql.ts
@@ -1,8 +1,16 @@
 import { gql, useMutation } from "@apollo/client";
+import { format } from "date-fns";
+import client from "../../config/apoloConfig";
 import {
+  GetPlannerRecipeByDateQuery,
+  GetPlannerRecipeByDateQueryVariables,
   OverWritePlannerByDatesMutation,
   OverWritePlannerByDatesMutationVariables,
+  Planner_Insert_Input,
 } from "../../generated/graphql";
+import { IRecipeToModify } from "../../redux/Planner/PlannerItemModalSlice";
+import { store } from "../../redux/store";
+import { GET_PLANNER_RECIPE_BY_DATE } from "../query/useGetPlannerRecipeByDate";
 
 export const OVER_WRITE_PLANNER_BY_DATES = gql`
   mutation OverWritePlannerByDates(
@@ -30,5 +38,123 @@ export default function useOverWritePlannerByDates() {
     OverWritePlannerByDatesMutationVariables
   >(OVER_WRITE_PLANNER_BY_DATES);
 
-  return { overWritePlannerByDates, data, error, client };
+  const { recipeToModify } = store.getState().PlannerItemModalSlice;
+
+  const handleSelectDay = async (date: string) => {
+    const f = "yyyy-MM-dd";
+    // get from date and to date and format them
+    const fromDate = format(new Date(recipeToModify!.date!), f);
+    const toDate = format(new Date(date), f);
+
+    // get all recipe ids and arrange them into ordered array
+    const objects = [
+      ...getRecipesInPlanner(fromDate, recipeToModify!, true),
+      ...getRecipesInPlanner(toDate, recipeToModify!),
+    ];
+    // combine 2 groups of recipes
+    // run query
+    overWritePlannerByDates({
+      variables: { dates: [fromDate, toDate], objects },
+      optimisticResponse: {
+        delete_planner: {
+          affected_rows: 0,
+          __typename: "planner_mutation_response",
+        },
+        insert_planner: {
+          returning: objects.map(({ date, index, recipe_id }) => {
+            return {
+              date,
+              index: index!,
+              recipe: {
+                id: recipe_id!,
+                __typename: "recipe",
+              },
+              __typename: "planner",
+            };
+          }),
+          __typename: "planner_mutation_response",
+        },
+        __typename: "mutation_root",
+      },
+      update: (cache, { data }) => {
+        if (!data || !data.insert_planner) return;
+        const {
+          insert_planner: { returning },
+        } = data;
+        cache.modify({
+          fields: {
+            [`planner({"where":{"date":{"_eq":"${toDate}"}}})`]: (
+              curr,
+              { toReference }
+            ) => {
+              return [
+                ...returning
+                  .filter((elem) => elem.date === toDate)
+                  .map((elem) => {
+                    return {
+                      ...elem,
+                      recipe: toReference(elem.recipe),
+                    };
+                  }),
+              ];
+            },
+            [`planner({"where":{"date":{"_eq":"${fromDate}"}}})`]: (
+              curr,
+              { toReference }
+            ) => {
+              return [
+                ...returning
+                  .filter((elem) => elem.date === fromDate)
+                  .map((elem) => {
+                    return {
+                      ...elem,
+                      recipe: toReference(elem.recipe),
+                    };
+                  }),
+              ];
+            },
+          },
+        });
+      },
+    });
+  };
+
+  return { handleSelectDay, data, error, client };
+}
+
+function getRecipesInPlanner(
+  date: string,
+  { id, index }: IRecipeToModify,
+  excludeSelectedRecipe: boolean = false
+): Planner_Insert_Input[] {
+  const cache = client.readQuery<
+    GetPlannerRecipeByDateQuery,
+    GetPlannerRecipeByDateQueryVariables
+  >({ query: GET_PLANNER_RECIPE_BY_DATE, variables: { date } });
+
+  if (!cache) return [];
+
+  const { planner } = cache;
+  type plannerElem = typeof planner[0]; // shut up typescript
+
+  const filtered = planner.filter(({ recipe, index: i }: plannerElem) => {
+    // remove the recipe by pk of id and idex
+    if (excludeSelectedRecipe) {
+      return i !== index;
+    } else {
+      return true;
+    }
+  });
+
+  const recipeIdsSorted = filtered
+    .sort((a: plannerElem, b: plannerElem) => a.index - b.index)
+    .map(({ recipe }: plannerElem) => recipe.id);
+
+  if (!excludeSelectedRecipe) {
+    recipeIdsSorted.push(id);
+  }
+
+  return recipeIdsSorted.map((id, index) => {
+    return { date, index, recipe_id: id };
+  });
 }
